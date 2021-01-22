b8c9f7eba1c49b6032dbcbc0aa442e9a3af23ed8
eminebülbül <emn.bulbul@hotmail.com>
2021-01-22 03:52:52 +0300

    16 ocak 2021

---
+++ DeğerVeReferansTipler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeğerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
